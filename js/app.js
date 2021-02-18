d678421fbc11b1a1c2044aea3a1bc2156b645def
Jelena Feliciano <jfel217@gmail.com>
2021-02-18 18:57:35 -0500

    js file created and added to index
    created the js files with some notes on where to start building

---
+++ js/app.js
@@ -0,0 +1,43 @@
+
+/**
+ * Global Variables for creating DOM elements 
+ * 
+*/
+//create a div with class modal-container set to template literals 
+    //`
+        //<div class="modal">
+        //  <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button> 
+        //  <div class="modal-info-container">
+        //  <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
+
+            //</div>
+        //</div>
+    //`
+    //create a variable modalDiv set to createElement div 
+        //give it className modal 
+    //append div.modal to div.modal-container
+        //create a button 
+            //class name modal-close-btn
+            //type attribute button 
+            //id modal-close-btn
+    //create a div with class modal-info-container
+/**
+ * Fetch functions
+ * 
+*/
+
+/**
+ * Helper functions
+ * 
+*/
+
+/**
+ * Event listeners 
+ * 
+*/
+
+
+/**
+ * post data
+ * 
+*/
