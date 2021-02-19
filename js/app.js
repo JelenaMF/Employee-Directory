7ee9c8502fa843737fbd1c2985afffc95145fcbc
Jelena Feliciano <jfel217@gmail.com>
2021-02-19 00:32:37 -0500

    Update app.js
    rewrote the created elements into template literals to make it easier to read.

--- js/app.js
+++ js/app.js
@@ -5,60 +5,74 @@
 */
 //search markup
 const searchDiv = document.querySelector('.search-container');
-console.log(searchDiv);
-const myForm = document.createElement('form');
-myForm.action = "#";
-myForm.method = "GET"
-searchDiv.appendChild(myForm);
+searchDiv.insertAdjacentHTML('beforeend', `
+    <form action="#" method="GET">
+        <input type="search" id="search-input"
+        class="search-input" placeholder="Search...">
+        <input type="submit" value="Search" id="search-submit" class="search-submit">
+    </form>
+`)
+ console.log(searchDiv);
 
-const searchInput = document.createElement('input');
-searchInput.type = "search";
-searchInput.id = "search-input";
-searchInput.className = "search-input";
-searchInput.placeholder = "Search...";
-myForm.appendChild(searchInput);
-const submitSearch = document.createElement('input');
-submitSearch.type = "submit";
-submitSearch.value = "Search";
-submitSearch.id = 'search-submit';
-submitSearch.className = 'search-submit';
-myForm.appendChild(submitSearch);
+//gallary markup
+const gallery = document.getElementById('gallery');
+console.log(gallery);
+gallery.insertAdjacentHTML('beforeend', `
+    <div class="card">
+        <div class="card-img-container">
+            <img class="card-img" src="https://placehold.it/90x90" alt="profile picture"> 
+        </div>
+        <div class="card-info-container">
+            <h3 id="name" class="card-name cap">first last</h3>
+            <p class="card-text">email</p>
+            <p class="card-text cap">city, state, </p>
+        </div>
+    </div>
+`)
 
+//modal markup
+const modalContainer = document.createElement('div');
+document.querySelector('body').appendChild(modalContainer);
+console.log(modalContainer);
+modalContainer.insertAdjacentHTML('beforeend', `
+    <div class="modal">
+        <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+        <div class="modal-info-container">
+        <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
+        <h3 id="name" class="modal-name cap">name</h3>
+            <p class="modal-text">email</p> 
+            <p class="modal-text cap">city</p>
+            <hr>
+            <p class="modal-text">(555)-555-5555</p>
+            <p class="modal-text">Birhday: 10/21/2015</p>
+        </div>
+    </div>
+`)
 
-//create a div with class modal-container set to template literals 
-    //`
-        //<div class="modal">
-        //  <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button> 
-        //  <div class="modal-info-container">
-        //  <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
-        //  <h3 id="name" class="modal-name cap">name</h3>
-        //      <p class="modal-text">email</p>
-           //   <p class="modal-text cap">city</p>
-            //  <hr>
-            //  <p class="modal-text">(555) 555-5555</p>
-            //  <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
-             // <p class="modal-text">Birthday: 10/21/2015</p>
-            //</div>
-        //</div>
-    //`
-    //create a variable modalDiv set to createElement div 
-        //give it className modal 
-    //append div.modal to div.modal-container
-        //create a button 
-            //class name modal-close-btn
-            //type attribute button 
-            //id modal-close-btn
-    //create a div with class modal-info-container
 /**
  * Fetch functions
  * 
 */
+function fetchData(url) {
+    return fetch(url)
+    .then(checkStatus)
+    .then(data => (console.log(data)))
+}
 
+Promise.all([
+    fetchData('https://randomapi.com/api/6de6abfedb24f889e0b5f675edc50deb?fmt=raw&sole')
+])
 /**
  * Helper functions
  * 
 */
-
+function checkStatus(res) {
+    if(res.ok) {
+        return Promise.resolve(res)
+    } else {
+        return Promise.reject(new Error(res.statusText))
+    }
+}
 /**
  * Event listeners 
  * 
